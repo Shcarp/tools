cdfd453b832fcb21b16a82cbd7f9573e406227fa
codeDaiQin <1046572917@qq.com>
2023-03-26 18:35:12 +0800

    feat: 🔥 base config

--- src/page/edit/index.tsx
+++ src/page/edit/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 // import "./styles.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <div>111</div>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
