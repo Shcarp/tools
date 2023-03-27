62f7cfa04ff079f077b360d23b67319fe9c257ee
sunhui1 <sunuih@qq.com>
2023-03-27 20:30:13 +0800

    格式化一下

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,38 @@
-// import { readdirSync } from 'fs'
-import path from 'path'
-import { defineConfig, normalizePath } from 'vite'
+import path from "path";
+import { defineConfig, normalizePath } from "vite";
 // import { createHtmlPlugin } from 'vite-plugin-html'
-import virtualHtmlTemplate from 'vite-plugin-virtual-html-template'
-import react from '@vitejs/plugin-react'
-import autoprefixer from 'autoprefixer'
-import eslint from 'vite-plugin-eslint'
+import virtualHtmlTemplate from "vite-plugin-virtual-html-template";
+import react from "@vitejs/plugin-react";
+import autoprefixer from 'autoprefixer';
+import eslint from "vite-plugin-eslint";
+import { readdirSync, accessSync, constants } from "fs";
 
-const variablePath = normalizePath(path.resolve('src/global/common.less'))
+const variablePath = normalizePath(path.resolve("src/global/common.less"));
 
-// const pages = readdirSync(path.resolve(__dirname, "src/page")).map((pageName) => {
-//   return {
-//     // entry: path.resolve(__dirname, `../../src/page/${pageName}/index.tsx`),
-//     entry: `src/page/${pageName}/index.tsx`,
-//     template: "public/index.html",
-//     filename: `${pageName}.html`,
-//     injectOptions: {
-//       data: {
-//         title: pageName,
-//       },
-//     },
-//   }
-// })
-// console.log(pages)
+const title = {
+  edit: '编辑',
+}
+
+const pages = readdirSync(path.resolve(__dirname, "src/page")).reduce(
+  (pre, cur) => {
+    try {
+      accessSync(path.resolve(__dirname, `./src/page/${cur}/index.tsx`), constants.F_OK)
+      pre[0][cur] = {
+        template: 'public/index.html',
+        title: title[cur] ?? cur,
+        entry: `src/page/${cur}/index.tsx`,
+      };
+      pre[1][cur] = {
+        [cur]: path.resolve(__dirname, `${cur}.html`),
+      };
+
+    } catch (error) {
+    }
+    return pre;
+    
+  },
+  [{}, {}]
+);
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
@@ -30,18 +40,7 @@ export default defineConfig(async () => ({
     eslint(),
     react({ fastRefresh: false }),
     virtualHtmlTemplate({
-      pages: {
-        index: {
-          template: 'public/index.html',
-          title: 'main',
-          entry: 'src/page/index/index.tsx',
-        },
-        edit: {
-          template: 'public/index.html',
-          title: 'edit',
-          entry: 'src/page/edit/index.tsx',
-        },
-      },
+      pages: pages[0],
     }),
   ],
   css: {
@@ -54,7 +53,7 @@ export default defineConfig(async () => ({
       plugins: [
         autoprefixer({
           // 指定目标浏览器
-          overrideBrowserslist: ['Chrome > 40', 'ff > 31', 'ie 11'],
+          overrideBrowserslist: ["Chrome > 40", "ff > 31", "ie 11"],
         }),
       ],
     },
@@ -70,20 +69,17 @@ export default defineConfig(async () => ({
   },
   // to make use of `TAURI_DEBUG` and other env variables
   // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
-  envPrefix: ['VITE_', 'TAURI_'],
+  envPrefix: ["VITE_", "TAURI_"],
   build: {
     // Tauri supports es2021
-    target: process.env.TAURI_PLATFORM === 'windows' ? 'chrome105' : 'safari13',
+    target: process.env.TAURI_PLATFORM === "windows" ? "chrome105" : "safari13",
     // don't minify for debug builds
-    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
     // produce sourcemaps for debug builds
     sourcemap: !!process.env.TAURI_DEBUG,
     emptyOutDir: true,
     rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-        edit: path.resolve(__dirname, 'edit.html'),
-      },
+      input: pages[1],
     },
   },
-}))
+}));
