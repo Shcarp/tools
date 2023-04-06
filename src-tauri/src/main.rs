19d4ee536b7fb91ce8162912e1991a68520b5585
sunhui1 <sunuih@qq.com>
2023-04-06 23:40:17 +0800

    修改名称

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use notes::{lmian};
+use tools::{lmian};
 
 fn main() {
     lmian()
